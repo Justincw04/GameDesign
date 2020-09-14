ff3fb50a2410d1c5d040d91abf3a81711fe23062
Justin <Justin@MacBook-Pro.local>
2020-09-14 10:29:32 -0500

    Data and Variables work

---
+++ Chapter 1/addingNumbers.py
@@ -0,0 +1,14 @@
+# Justin Cortez Wartell
+str='Hello World!'
+counter=100
+miles=1000
+name="John"
+print(counter)
+print(miles)
+print(name)
+print(str)
+print(str[0])
+print(str[2:5])
+print(str[2:])
+print(str * 2)
+print(str + "TEST")

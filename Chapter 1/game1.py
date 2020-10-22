0b26a6909da20fb36403e9bfe1cda9ff1c92c715
Justin <Justin@Justins-Macbook-Pro.local>
2020-10-22 10:29:29 -0500

    game

--- Chapter 1/game1.py
+++ Chapter 1/game1.py
@@ -5,21 +5,61 @@ name=input("What is your name? ")
 print("Good luck! ", name)
 gameWords=['python', 'java','PHP','javaScript', 'computer', 'geeks', 'keyboard','laptop', 'headphone', 'hardware','software' ]
 # use the choice method of my random fucntion to pick a gameWords
-answer=input('do you want to guess a word ')
-
+answer=input('do you want to guess a word? ')
+win = "no"
+checkWord = ""
 while answer == 'yes':
     word= random.choice(gameWords)
     guesses =''
-    turns=10
-while turns>0:
-    for char in word:
-            if char in guesses:
-                print(char)
+    turns=5
+    win = 'no'
+
+    while turns>0 and win == 'no':
+        if checkWord !=word:
+            for char in word:
+                if char in guesses:
+                    print(char, end = '')
+                    checkWord += char
+                else:
+                    print('_', end = '')
+                    checkWord += '_'
+        print()
+
+        if checkWord == word:
+            print('you win!')
+            win = yes
+        else:
+            guess = input("guess a letter or word. ")
+
+            guesses += guess
+
+            if guess in word:
+                print('you have', turns, 'extra chances left.')
             else:
-                print('_')
-guess= input("give me a letter: ")
-guesses += guess
+                turns-=1
+                print('you have', turns, 'extra chances left.')
+        if checkWord == word:
+            win = 'yes'
+        else:
+            checkWord = ''
+    else:
+        turns = 0
+        if word in final:
+            print('you win')
+            print("the word is: "+ word)
+            break
+        guess= input("give me a letter: ")
+        print()
+        count=0
+        if guess not in guesses:
+            count=word.count(guess[0])
+            if guess not in word:
+                turns=turns-1
+        guesses += guess[0]
 turns-=1
 
 answer=input('do you want to play again ')
 time.sleep(5)
+
+if not answer is not"yes":
+    print('thanks for playing')
---
+++ Chapter 1/hangman.py
@@ -0,0 +1,40 @@
+import random
+import time
+
+
+
+name=input("What is your name? ")
+print("Good Luck !", name)
+gameWords = ['salmon','stirfry','meatball','tofu','hamburger','hotdog','chicken','steak','sushi','cook','fork','knife','spoon','cake','soup','salad']
+answer=input('do you want to guess a word')
+
+
+
+while answer == 'yes':
+    word= random.choice(gameWords)
+    guesses =''
+    final =''
+    turns=5
+    while turns>0:
+        for char in word:
+            if char in guesses:
+                print(char,end ='')
+                final= final+char
+            else:
+                print('_', end =' ')
+        print(' ')
+        if word in final:
+            print('you win')
+            print("the word is: "+ word)
+            break
+        guess= input("give me a letter: ")
+        guesses += guess
+        if guess not in word:
+            turns -=1
+            print("you have ",end= '')
+            print(turns)
+        if turns == 0:
+            print('GAME OVER!')
+            break
+        answer= input('do you want to play again')
+    time.sleep(5)

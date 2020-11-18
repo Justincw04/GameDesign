fbc6e6557345fb0c00278059bcafed0a2179743b
Justin <Justin@Justins-Macbook-Pro.local>
2020-11-18 00:22:09 -0600

    pygame work

---
+++ Chapter 1/pygameBasics.py
@@ -0,0 +1,62 @@
+#Justin Cortez Wartell
+import pygame
+import time
+import sys
+#asking for color
+pygame.init()
+pygame.time.delay(100)
+screen=pygame.display.set_mode((800,800))
+white=[255,255,255]
+red=[255,0,0]
+green=[0,255,0]
+screen.fill(red)
+pygame.display.set_caption("My game")
+pygame.display.update()
+pygame.time.delay(1000)
+screen.fill(white)
+pygame.display.set_caption("Your game ")
+pygame.display.flip()
+running = True
+while running:
+    for i in pygame.event.get():
+        if i.type == pygame.QUIT:
+            running = False
+    pygame.draw.rect(screen, )
+pygame.quit()
+
+
+
+
+
+
+
+
+pygame.time.delay(100)
+#                window    color       x, y   w h
+pygame.draw.rect(screen,(10,123,10),(20,20 50,100))
+pygame.display.update()
+#we are going to move our rectangle background
+keyBoardKey=pygame.key.get_pressed()
+#check what key is get_pressed
+speed=1
+if keyBoardKey[pygame_.K_LEFT]:
+    x-= speed
+
+
+if keyBoardKey[pygame_.K_RIGHT]:
+    x+= speed
+
+
+if keyBoardKey[pygame_.K_UP]:
+    y-= speed
+
+
+if keyBoardKey[pygame_.K_DOWN]:
+    y+= speed
+
+
+
+#                 windown   color       position, radius, thick
+#pygame.draw.circle(screen,(0,120,129), (100,100),30, 4)
+#pygame.display.update()
+pygame.quit()
---
+++ Chapter 1/pygameTest.py
@@ -0,0 +1,74 @@
+#Justin Cortez Wartell
+
+#Pygame work
+#background color
+import pygame, time, sys
+#asking the user for a color
+pygame.init()
+#Background picture should be the size of your screen
+backdrop=pygame.image.load('Background.png')
+tempSprite=pygame.image.load('dht11.jpg')
+pygame.time.delay(100)
+WIDTH=1000
+HEIGHT=800
+screen=pygame.display.set_mode((WIDTH,HEIGHT))
+white=[255,255,255]
+red=[255,0,0]
+green = [0,255,0]
+
+screen.fill(white)
+pygame.display.set_caption("My Shapes")
+pygame.display.flip()
+running = True
+#VAriables to control objects
+x=29
+y=20
+w=32
+h=64
+r=30
+Jump=False
+high=10
+def redrawWindow():
+    screen.blit(backdrop, (0,0))
+    screen.blit(tempSprite,(x,y))
+    pygame.draw.rect(screen,(10,123,10),(x,y,w,h))
+    pygame.display.update()
+
+while running:
+    #screen.fill(white)
+    #Background image on the screen
+
+    for i in pygame.event.get():
+        if i.type == pygame.QUIT:
+            running = False
+    pygame.time.delay(100)
+    #                window    color     x, y   w   h
+
+    # We are going to move our rectnagle around
+    keyBoardKey=pygame.key.get_pressed()
+    #check what key was pressed
+    speed = 10
+    if keyBoardKey[pygame.K_LEFT] and x > speed:  # I need to substract from x
+        x -= speed
+        r -= speed
+    if keyBoardKey[pygame.K_RIGHT] and x < WIDTH - w - speed:  # I need to add to the x
+        x += speed
+        r += speed
+    if not(Jump): #this is for moving y without a jump
+        if keyBoardKey[pygame.K_UP] and y > speed:     # I need to substract to the y
+            y -= speed
+        if keyBoardKey[pygame.K_DOWN] and y < HEIGHT - h - speed:    # I need to add to the y
+            y += speed
+        if keyBoardKey[pygame.K_SPACE]:
+            Jump=True
+    else:
+        if high >= -10:
+            y -= (high * abs(high)) /2
+            high -= 1
+        else:
+            high = 10
+            jump = False
+
+    redrawWindow()
+
+pygame.quit()

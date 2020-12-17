e36109661f2cfddbf752b7041e4d6a317b0ad642
Justin <Justin@Justins-Macbook-Pro.local>
2020-12-17 10:16:27 -0600

    final

--- Chapter 1/snakeGame.py
+++ Chapter 1/snakeGame.py
@@ -8,15 +8,19 @@ pygame.init()
 
 b = 20
 a = 20
+
+#colors for later use
 white = (255, 255, 255)
 yellow = (255, 255, 102)
 black = (0, 0, 0)
 red = (213, 50, 80)
 green = (0, 255, 0)
 blue = (50, 153, 213)
+orange = (255, 165, 0)
+dgreen = (0, 100, 0)
 
 RADIUS = 10
-WIDTH = 1600
+WIDTH = 1400
 HEIGHT = 800
 run = True
 
@@ -25,20 +29,22 @@ screen = pygame.display.set_mode((WIDTH, HEIGHT))
 pygame.display.set_caption('Snake Game')
 
 clock = pygame.time.Clock()
+#making fonts
 LETTER_FONT = pygame.font.SysFont('comicsans', 40)
 WORD_FONT = pygame.font.SysFont('comicsans', 60)
 TITLE_FONT = pygame.font.SysFont('comicsans', 70)
 #menu with instruction list and difficulty selector
 def menu():
     global click
-    screen.fill(white)
+    screen.fill(blue)
     text = TITLE_FONT.render("Let's Play Snake!", 1, black)
 
     #instructions
-    text = LETTER_FONT.render("In this game, use your arrow keys to move the direction of your snake. Eat food to grow longer and earn points.", 1, black)
-    text2 = LETTER_FONT.render("If you hit a wall or hit your own snake, you will lose", 1, black)
+    text = LETTER_FONT.render("In this game, use your arrow keys to move the direction of your snake. Eat food to grow", 1, black)
+    text2 = LETTER_FONT.render("longer and earn points. If you hit a wall or hit your own snake, you will lose", 1, black)
     text3 = LETTER_FONT.render("First, choose your difficulty level. The higher the difficulty level, the faster your snake moves", 1, black)
     text4 = LETTER_FONT.render("After you finish, your score will be stored into the leaderboards. Good Luck!", 1, black)
+    #positioning of text
     screen.blit(text, (20,20))
     screen.blit(text2, (20,60))
     screen.blit(text3, (20,100))
@@ -46,13 +52,14 @@ def menu():
     #difficulty choices
     choice = WORD_FONT.render("DIFFICULTY:",1,black)
     screen.blit(choice,(60, 200))
-    #3 circles for 3 difficulty levels
+    #4 rectangles for 3 difficulty levels and 1 quit button
     button1 = pygame.Rect(60,310,30,30)
     button2 = pygame.Rect(60,410,30,30)
     button3 = pygame.Rect(60,510,30,30)
     button4 = pygame.Rect(60,610,30,30)
+    #finding position of mouse
     mx, my = pygame.mouse.get_pos()
-
+    #4 rectangles, each the same size but different color and position
     pygame.draw.rect(screen, green, button1)
     pygame.draw.rect(screen, yellow, button2)
     pygame.draw.rect(screen, red, button3)
@@ -76,6 +83,7 @@ def menu():
     choice3=TITLE_FONT.render("Hard",1,red)
     screen.blit(choice3,(160,500))
 
+    #quit button
     quitmenu=TITLE_FONT.render("4",1,black)
     screen.blit(quitmenu,(100,600))
     choice4=TITLE_FONT.render("Quit",1,black)
@@ -85,9 +93,12 @@ def menu():
 
     event = pygame.event.poll()
 
+    #detect if mouse is clicked
     if event.type == pygame.MOUSEBUTTONDOWN:
         if event.button == 1:
             click = True
+    #detect if mouse position collides with position of rectangles,
+    #rectangles correspond with game function of difficulty
     if button1.collidepoint((mx,my)):
         if click:
             gameLoopEasy()
@@ -103,6 +114,7 @@ def menu():
     if button4.collidepoint((mx,my)):
         if click:
             pygame.quit()
+    #other options to quit besides clicking the button
     keyBoardKey = pygame.key.get_pressed()
     if keyBoardKey[pygame.K_ESCAPE]:
         pygame.quit()
@@ -116,17 +128,17 @@ def menu():
 pygame.time.delay(5000)
 snake_block = 10
 
-
+#other fonts
 font_style = pygame.font.SysFont("bahnschrift", 25)
 score_font = pygame.font.SysFont("comicsansms", 35)
 
-
+#function for score number
 def Your_score(score):
     value = score_font.render("Your Score: " + str(score), True, yellow)
     screen.blit(value, [0, 0])
 
 
-
+#function for snake
 def our_snake(snake_block, snake_list):
     for x in snake_list:
         pygame.draw.rect(screen, black, [x[0], x[1], snake_block, snake_block])
@@ -136,8 +148,9 @@ def message(msg, color):
     mesg = font_style.render(msg, True, color)
     screen.blit(mesg, [WIDTH / 6, HEIGHT / 3])
 
-
+#easy difficulty/low speed
 def gameLoopEasy():
+    #speed of snake
     snake_speed = 15
     game_over = False
     game_close = False
@@ -151,17 +164,19 @@ def gameLoopEasy():
     snake_List = []
     Length_of_snake = 1
 
+    #coordinates of food
     foodx = round(random.randrange(0, WIDTH - snake_block) / 10.0) * 10.0
     foody = round(random.randrange(0, HEIGHT - snake_block) / 10.0) * 10.0
 
     while not game_over:
 
         while game_close == True:
+            #lose message
             screen.fill(blue)
             message("You Lost! Press C-Play Again or Q-Quit", red)
             Your_score(Length_of_snake - 1)
             pygame.display.update()
-
+            #2 options: quit or play again
             for event in pygame.event.get():
                 if event.type == pygame.KEYDOWN:
                     if event.key == pygame.K_q:
@@ -177,7 +192,7 @@ def gameLoopEasy():
                                 run = False
                                 sys.exit()
 
-
+        #commands to make snake move with keys
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 game_over = True
@@ -199,7 +214,8 @@ def gameLoopEasy():
             game_close = True
         x1 += x1_change
         y1 += y1_change
-        screen.fill(blue)
+        screen.fill(dgreen)
+        #drawing everything
         pygame.draw.rect(screen, green, [foodx, foody, snake_block, snake_block])
         snake_Head = []
         snake_Head.append(x1)
@@ -211,7 +227,7 @@ def gameLoopEasy():
         for x in snake_List[:-1]:
             if x == snake_Head:
                 game_close = True
-
+        #scores
         our_snake(snake_block, snake_List)
         Your_score(Length_of_snake - 1)
 
@@ -227,7 +243,7 @@ def gameLoopEasy():
     pygame.quit()
     quit()
 
-
+#same code but faster speed for medium difficulty
 def gameLoopMedium():
     snake_speed = 20
     game_over = False
@@ -289,7 +305,7 @@ def gameLoopMedium():
             game_close = True
         x1 += x1_change
         y1 += y1_change
-        screen.fill(yellow)
+        screen.fill(orange)
         pygame.draw.rect(screen, green, [foodx, foody, snake_block, snake_block])
         snake_Head = []
         snake_Head.append(x1)
@@ -316,7 +332,7 @@ def gameLoopMedium():
 
     pygame.quit()
     quit()
-
+#same code but for faster hard difficulty
 def gameLoopHard():
     snake_speed = 25
     game_over = False
@@ -406,7 +422,7 @@ def gameLoopHard():
 
     pygame.quit()
     quit()
-
+#code to make game run
 while run:
     menu()
     pygame.display.update()
